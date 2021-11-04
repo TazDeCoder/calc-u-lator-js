a9cefa27c7593e73ede877b58b62cfe8d4b19fe0
tarwat-uddin <t.uddin9121@gmail.com>
2021-11-04 13:52:46 +0000

    Refractor app.js
    Minor improvements

--- app.js
+++ app.js
@@ -45,10 +45,9 @@ function init() {
   // Reset conditons
   currOper = "";
   currNum = 0;
-  // Clean-up GUI
-  const [...btns] = keypadAside.querySelectorAll(".keypad__btn");
-  btns.forEach((btn) => btn.classList.remove("keypad__btn--active"));
+  // Clean-up UI
   labelDisplay.textContent = "0";
+  deactivateOperators();
 }
 
 ////////////////////////////////////////////////
@@ -62,24 +61,25 @@ function deactivateOperators() {
   btns.forEach((btn) => btn.classList.remove("keypad__btn--active"));
 }
 
-function keypadTriggered(val) {
+function keypadTriggered() {
+  let val;
   const displayStr = labelDisplay.textContent;
-  if (!btnEquals.classList.contains("keypad__btn--active")) {
-    if (displayStr === "0") updateDisplay(val);
-    else {
-      // Prevents > 1 decimal occurance in input string
-      // and displayLbl exceeding > 13 chars
-      if (!(displayStr.includes(".") && val === ".") && displayStr.length <= 13)
-        updateDisplay(displayStr.concat(val));
-    }
-    deactivateOperators();
-  }
+  if (displayStr === "0") val = this;
+  else if (
+    !(displayStr.includes(".") && this === ".") &&
+    displayStr.length <= 13
+    // Prevents > 1 decimal occurance in input string
+    // and labelDisplay exceeding > 13 chars
+  )
+    val = displayStr.concat(this);
+  updateDisplay(val);
+  deactivateOperators();
 }
 
 function operatorTriggered() {
   currOper = this;
   if (currNum && !btnEquals.classList.contains("keypad__btn--active"))
-    calcNumbers(currOper);
+    calcNumbers(currOper.value);
   else currNum = +labelDisplay.textContent;
 
   deactivateOperators();
@@ -89,10 +89,10 @@ function operatorTriggered() {
 
 function displayCalc() {
   if (currNum) {
-    calcNumbers(currOper);
+    calcNumbers(currOper.value);
     deactivateOperators();
-    btnEquals.classList.add("keypad__btn--active");
     btnEquals.blur();
+    btnEquals.classList.add("keypad__btn--active");
     updateDisplay(currNum);
   }
 }
@@ -103,8 +103,7 @@ function displayCalc() {
 
 function calcNumbers(oper) {
   const tempNum = +labelDisplay.textContent;
-  console.log(currNum, tempNum);
-  switch (oper.value) {
+  switch (oper) {
     case "+":
       currNum = calculator.add(currNum, tempNum);
       break;
@@ -124,6 +123,18 @@ function calcNumbers(oper) {
 ////// Event Handlers
 ///////////////////////////////////////////////
 
+function tapCallBack(e) {
+  const clicked = e.target;
+  if (!clicked) return;
+  clicked.blur();
+  if (clicked.classList.contains("keypad__btn"))
+    clicked.classList.toggle("keypad__btn--active");
+}
+
+// Emulates visual tap feedback
+keypadDock.addEventListener("mousedown", tapCallBack);
+keypadDock.addEventListener("mouseup", tapCallBack);
+
 keypadDock.addEventListener("click", function (e) {
   const clicked = e.target;
   if (!clicked) return;
@@ -131,7 +142,7 @@ keypadDock.addEventListener("click", function (e) {
     clicked.classList.contains("keypad__btn") &&
     !clicked.classList.contains("keypad__btn--clear")
   )
-    keypadTriggered(clicked.value);
+    keypadTriggered.call(clicked.value);
 });
 
 keypadAside.addEventListener("click", function (e) {
@@ -150,18 +161,18 @@ btnEquals.addEventListener("click", displayCalc);
 // --- KEYBOARD SUPPORT ---
 
 document.addEventListener("keyup", function (e) {
-  const keypress = e.key;
+  const pressed = e.key;
+  if (!pressed) return;
   // Number keyboard input [0 - 9]
-  if (keypress.match(/\d/g)) return keypadTriggered(keypress);
-  // General keyboard input use case
-  if (!keypress) return;
-  switch (keypress) {
-    case "Escape":
-      return init();
+  if (pressed.match(/\d/g)) return keypadTriggered.call(pressed);
+  // Keyboard input use cases
+  switch (pressed) {
     case "Backspace":
       return updateDisplay(labelDisplay.textContent.slice(0, -1));
     case "Enter":
-      return displayCalc;
+      return displayCalc();
+    case "=":
+      return displayCalc();
     case "+":
       return operatorTriggered.call(btnPlus);
     case "-":
@@ -171,6 +182,8 @@ document.addEventListener("keyup", function (e) {
     case "/":
       return operatorTriggered.call(btnDivide);
     case ".":
-      return keypadTriggered(keypress);
+      return keypadTriggered.call(pressed);
+    case "Escape":
+      return init();
   }
 });
