76372b02f85edee2484abeb5f5455905d789d5db
tarwat-uddin <t.uddin9121@gmail.com>
2021-12-13 12:14:55 +0000

    Refractor app.js
    Major improvements

--- app.js
+++ app.js
@@ -1,212 +1,213 @@
 "use strict";
 
 ////////////////////////////////////////////////
-////// Selecting HTML elements
+////// Selecting HTML Elements
 ///////////////////////////////////////////////
 
+// Parents
+const keypadDock = document.querySelector(".keypad__item--dock");
+const keypadAside = document.querySelector(".keypad__item--aside");
+const asideHistory = document.querySelector(".aside__content--history");
 // Labels
 const labelDisplay = document.querySelector(".keypad__label--display");
 // Buttons
-const btnPlus = document.querySelector(".keypad__btn--plus");
-const btnMinus = document.querySelector(".keypad__btn--minus");
-const btnTimes = document.querySelector(".keypad__btn--times");
-const btnDivide = document.querySelector(".keypad__btn--divide");
-const btnEquals = document.querySelector(".keypad__btn--equals");
-const btnClearCalc = document.querySelector(".keypad__btn--clear");
+const btnPlus = document.querySelector(".item__btn--plus");
+const btnMinus = document.querySelector(".item__btn--minus");
+const btnTimes = document.querySelector(".item__btn--times");
+const btnDivide = document.querySelector(".item__btn--divide");
+const btnEquals = document.querySelector(".item__btn--equals");
+const btnClearCalc = document.querySelector(".item__btn--clear");
 const btnClearHist = document.querySelector(".aside__btn--clear");
-// Parents
-const keypadDock = document.querySelector(".keypad--dock");
-const keypadAside = document.querySelector(".keypad--aside");
-const asideHistory = document.querySelector(".aside--history");
 
 ////////////////////////////////////////////////
-////// Global variables
+////// Public API Modules
 ///////////////////////////////////////////////
 
-let currNum, currOper;
+// Module that performs mathematical calculations
+const calculator = (function module() {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
 
-// Object that performs math calculations
-const calculator = {
-  add(num1, num2) {
-    return num1 + num2;
-  },
-  subtract(num1, num2) {
-    return num1 - num2;
-  },
-  multiply(num1, num2) {
-    return num1 * num2;
-  },
-  divide(num1, num2) {
-    return num1 / num2;
-  },
-};
+  var publicAPI = {
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+  };
 
-(() => init())();
-
-function init() {
-  clearCalc();
-  clearHist();
-}
+  return publicAPI;
+})();
 
 ////////////////////////////////////////////////
-////// App UI Setup
+////// App Architecture
 ///////////////////////////////////////////////
 
-const updateDisplay = (str) => (labelDisplay.textContent = str);
+class App {
+  #currNum;
+  #currOper;
 
-function deactivateOpers() {
-  const [...btns] = keypadAside.querySelectorAll(".keypad__btn");
-  btns.forEach((btn) => btn.classList.remove("keypad__btn--active"));
-}
+  constructor() {
+    this._loadApp();
+    // Add event handlers
+    document.addEventListener("keyup", this._handleKeyupPress.bind(this));
+    keypadDock.addEventListener("mousedown", this._handleDockEvents);
+    keypadDock.addEventListener("mouseup", this._handleDockEvents);
+    keypadDock.addEventListener("click", this._handleDockEvents.bind(this));
+    keypadAside.addEventListener("click", this._handleAsideEvents.bind(this));
+    btnEquals.addEventListener("click", this._displayCalc.bind(this));
+    btnClearCalc.addEventListener("click", this._clearCalc.bind(this));
+    btnClearHist.addEventListener("click", this._clearHist);
+  }
 
-function clearCalc() {
-  currOper = "";
-  currNum = 0;
-  labelDisplay.textContent = "0";
-  deactivateOpers();
-}
+  _loadApp() {
+    this._clearCalc();
+    this._clearHist();
+  }
 
-function clearHist() {
-  if (!asideHistory.firstChild) return;
-  while (asideHistory.firstChild)
-    asideHistory.removeChild(asideHistory.firstChild);
-}
+  _updateDisplay(str) {
+    if (str) labelDisplay.textContent = str;
+  }
 
-function keypadTriggered() {
-  if (btnEquals.classList.contains("keypad__btn--active")) return;
-  let val;
-  const displayStr = labelDisplay.textContent;
-  if (displayStr === "0") val = this;
-  else if (
-    !(displayStr.includes(".") && this === ".") &&
-    displayStr.length <= 13
-    // Prevents > 1 decimal occurance in input string
-    // and labelDisplay exceeding > 13 chars
-  )
-    val = displayStr.concat(this);
-  updateDisplay(val);
-  deactivateOpers();
-}
+  _displayCalc() {
+    if (this.#currNum) {
+      const [num1, num2] = this._calcNumbers(this.#currOper.value);
+      this._deactivateOpers();
+      btnEquals.blur();
+      btnEquals.classList.add("btn--active");
+      this._updateDisplay(this.#currNum);
+      this._updateHist(num1, num2, this.#currNum);
+    }
+  }
 
-function operTriggered() {
-  currOper = this;
-  if (currNum && !btnEquals.classList.contains("keypad__btn--active"))
-    calcNumbers(currOper.value);
-  else currNum = +labelDisplay.textContent;
+  _clearCalc() {
+    this.#currOper = "";
+    this.#currNum = 0;
+    labelDisplay.textContent = "0";
+    this._deactivateOpers();
+  }
 
-  deactivateOpers();
-  this.classList.add("keypad__btn--active");
-  updateDisplay("0");
-}
+  _clearHist() {
+    if (!asideHistory.firstChild) return;
+    while (asideHistory.firstChild)
+      asideHistory.removeChild(asideHistory.firstChild);
+  }
 
-function displayCalc() {
-  if (currNum) {
-    const [num1, num2] = calcNumbers(currOper.value);
-    deactivateOpers();
-    btnEquals.blur();
-    btnEquals.classList.add("keypad__btn--active");
-    updateDisplay(currNum);
-    updateHist(num1, num2, currNum);
+  _updateHist(num1, num2, num3) {
+    const p = document.createElement("p");
+    p.textContent = `${num1} ${this.#currOper.value} ${num2} = ${num3}`;
+    p.classList.add("content__label");
+    asideHistory.appendChild(p);
+  }
+
+  _calcNumbers(oper) {
+    var tempNum = +labelDisplay.textContent;
+    switch (oper) {
+      case "+":
+        this.#currNum = calculator.add(this.#currNum, tempNum);
+        break;
+      case "-":
+        this.#currNum = calculator.subtract(this.#currNum, tempNum);
+        break;
+      case "*":
+        this.#currNum = calculator.multiply(this.#currNum, tempNum);
+        break;
+      case "/":
+        this.#currNum = calculator.divide(this.#currNum, tempNum);
+        break;
+    }
+    return [this.#currNum, tempNum];
+  }
+
+  _keypadTriggered(keypad) {
+    if (btnEquals.classList.contains("btn--active")) return;
+    this._deactivateOpers();
+    const keypadVal = keypad?.value ?? keypad;
+    const displayStr = labelDisplay.textContent;
+    if (displayStr === "0") return this._updateDisplay(keypadVal);
+    if (
+      // Prevents > 1 decimal occurance in input string
+      // and labelDisplay exceeding > 13 chars
+      !(displayStr.includes(".") && keypadVal === ".") &&
+      displayStr.length <= 13
+    )
+      this._updateDisplay(displayStr.concat(keypadVal));
+  }
+
+  _deactivateOpers() {
+    const [...btns] = keypadAside.querySelectorAll(".item__btn");
+    btns.forEach((btn) => btn.classList.remove("btn--active"));
+  }
+
+  _operTriggered(oper) {
+    this.#currOper = oper;
+    if (this.#currNum && !btnEquals.classList.contains("btn--active"))
+      this._calcNumbers(this.#currOper.value);
+    else this.#currNum = +labelDisplay.textContent;
+
+    oper.classList.add("btn--active");
+    this._deactivateOpers();
+    this._updateDisplay("0");
+  }
+
+  _handleDockEvents(e) {
+    const clicked = e.target;
+    if (!clicked) return;
+
+    if (e.type === "mousedown" || e.type === "mouseup") {
+      clicked.blur();
+      if (clicked.classList.contains("item__btn"))
+        clicked.classList.toggle("btn--active");
+    }
+    if (e.type === "click") {
+      const clicked = e.target;
+      if (!clicked) return;
+      if (
+        clicked.classList.contains("item__btn") &&
+        !clicked.classList.contains("item__btn--clear")
+      )
+        this._keypadTriggered.call(this, clicked);
+    }
+  }
+
+  _handleAsideEvents(e) {
+    const clicked = e.target;
+    if (!clicked) return;
+    if (
+      clicked.classList.contains("item__btn") &&
+      !clicked.classList.contains("item__btn--equals")
+    )
+      this._operTriggered.call(this, clicked);
+  }
+
+  _handleKeyupPress(e) {
+    const key = e.key;
+    if (!key) return;
+    // Number keyboard input [0 - 9]
+    if (key.match(/\d/g)) return this._keypadTriggered.call(this, key);
+    // Keyboard input use cases
+    switch (key) {
+      case "Backspace":
+        return this._updateDisplay(labelDisplay.textContent.slice(0, -1));
+      case "Enter":
+        return this._displayCalc();
+      case "=":
+        return this._displayCalc();
+      case "+":
+        return this._operTriggered.call(this, btnPlus);
+      case "-":
+        return this._operTriggered.call(this, btnMinus);
+      case "*":
+        return this._operTriggered.call(this, btnTimes);
+      case "/":
+        return this._operTriggered.call(this, btnDivide);
+      case ".":
+        return this._keypadTriggered.call(this, key);
+      case "Escape":
+        return this._clearCalc();
+    }
   }
 }
 
-function updateHist(num1, num2, num3) {
-  const p = document.createElement("p");
-  p.textContent = `${num1} ${currOper.value} ${num2} = ${num3}`;
-  p.classList.add("aside__line");
-  asideHistory.appendChild(p);
-}
-
-////////////////////////////////////////////////
-////// App Logic
-///////////////////////////////////////////////
-
-function calcNumbers(oper) {
-  const tempNum = +labelDisplay.textContent;
-  const numArr = [currNum, tempNum];
-  switch (oper) {
-    case "+":
-      currNum = calculator.add(currNum, tempNum);
-      break;
-    case "-":
-      currNum = calculator.subtract(currNum, tempNum);
-      break;
-    case "*":
-      currNum = calculator.multiply(currNum, tempNum);
-      break;
-    case "/":
-      currNum = calculator.divide(currNum, tempNum);
-      break;
-  }
-  return numArr;
-}
-
-////////////////////////////////////////////////
-////// Event Handlers
-///////////////////////////////////////////////
-
-function tapCallBack(e) {
-  const clicked = e.target;
-  if (!clicked) return;
-  clicked.blur();
-  if (clicked.classList.contains("keypad__btn"))
-    clicked.classList.toggle("keypad__btn--active");
-}
-
-// Emulates visual tap feedback
-keypadDock.addEventListener("mousedown", tapCallBack);
-keypadDock.addEventListener("mouseup", tapCallBack);
-
-keypadDock.addEventListener("click", function (e) {
-  const clicked = e.target;
-  if (!clicked) return;
-  if (
-    clicked.classList.contains("keypad__btn") &&
-    !clicked.classList.contains("keypad__btn--clear")
-  )
-    keypadTriggered.call(clicked.value);
-});
-
-keypadAside.addEventListener("click", function (e) {
-  const clicked = e.target;
-  if (!clicked) return;
-  if (
-    clicked.classList.contains("keypad__btn") &&
-    !clicked.classList.contains("keypad__btn--equals")
-  )
-    operTriggered.call(clicked);
-});
-
-btnEquals.addEventListener("click", displayCalc);
-btnClearCalc.addEventListener("click", clearCalc);
-btnClearHist.addEventListener("click", clearHist);
-
-// --- KEYBOARD SUPPORT ---
-
-document.addEventListener("keyup", function (e) {
-  const pressed = e.key;
-  if (!pressed) return;
-  // Number keyboard input [0 - 9]
-  if (pressed.match(/\d/g)) return keypadTriggered.call(pressed);
-  // Keyboard input use cases
-  switch (pressed) {
-    case "Backspace":
-      return updateDisplay(labelDisplay.textContent.slice(0, -1));
-    case "Enter":
-      return displayCalc();
-    case "=":
-      return displayCalc();
-    case "+":
-      return operTriggered.call(btnPlus);
-    case "-":
-      return operTriggered.call(btnMinus);
-    case "*":
-      return operTriggered.call(btnTimes);
-    case "/":
-      return operTriggered.call(btnDivide);
-    case ".":
-      return keypadTriggered.call(pressed);
-    case "Escape":
-      return clearCalc();
-  }
-});
+const app = new App();
