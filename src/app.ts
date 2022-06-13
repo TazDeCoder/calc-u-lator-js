6269e925bfef4f192d44f768f8401532f0ab22da
Tarwat <t.uddin9121@gmail.com>
2022-06-13 17:39:12 +0100

    Migrated .js files to .ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 "use strict";
 
-import "core-js/stable";
-
 ////////////////////////////////////////////////
 ////// Selecting HTML Elements
 ///////////////////////////////////////////////
 
+// Labels
+const labelDisplay = document.querySelector(".keypad__label--display");
 // Parents
 const keypadDock = document.querySelector(".keypad__item--dock");
 const keypadAside = document.querySelector(".keypad__item--aside");
@@ -17,19 +17,17 @@ const btnTimes = document.querySelector(".item__btn--times");
 const btnDivide = document.querySelector(".item__btn--divide");
 const btnEquals = document.querySelector(".item__btn--equals");
 const btnClearHist = document.querySelector(".aside__btn--clear");
-// Labels
-const labelDisplay = document.querySelector(".keypad__label--display");
 
 ////////////////////////////////////////////////
 ////// Public API Modules
 ///////////////////////////////////////////////
 
 // Module that performs simple mathematical calculations
-const calculator = (function module() {
-  const add = (a, b) => a + b;
-  const subtract = (a, b) => a - b;
-  const multiply = (a, b) => a * b;
-  const divide = (a, b) => a / b;
+const calculator = (() => {
+  const add = (a: number, b: number) => a + b;
+  const subtract = (a: number, b: number) => a - b;
+  const multiply = (a: number, b: number) => a * b;
+  const divide = (a: number, b: number) => a / b;
 
   const publicAPI = {
     add: add,
@@ -45,9 +43,11 @@ const calculator = (function module() {
 ////// App Architecture
 ///////////////////////////////////////////////
 
+type operator = "+" | "-" | "*" | "/";
+
 class App {
-  #currNumber;
-  #currOperator;
+  currNumber: number;
+  currOperator: HTMLInputElement | null;
 
   constructor() {
     // Add event handlers
@@ -60,18 +60,18 @@ class App {
   /////////////////////////////////////
   //////////// Helper functions
 
-  _updateDisplay(str) {
-    labelDisplay.textContent = str;
+  _updateDisplay(str: string | number) {
+    labelDisplay.textContent = String(str);
   }
 
-  _deactivateBtns(btns) {
+  _deactivateBtns(btns: NodeListOf<Element>) {
     btns.forEach((btn) => btn.classList.remove("btn--active"));
   }
 
   _clearHist() {
     const labels = asideHistory.querySelectorAll(".content__label");
     labels.forEach((label) => label.classList.add("hidden"));
-    setTimeout(function () {
+    setTimeout(() => {
       asideHistory.innerHTML = "";
     }, 1000);
   }
@@ -79,48 +79,46 @@ class App {
   /////////////////////////////////////
   //////////// Handler functions
 
-  _handleDockEvents(e) {
-    const clicked = e.target.closest(".item__btn");
+  _handleDockEvents(e: Event) {
+    const clicked = (e.target as HTMLInputElement).closest(".item__btn");
     if (!clicked) return;
     // Number keypad triggered
     if (!clicked.classList.contains("item__btn--clear"))
       return this._keypadTriggered.call(this, clicked);
     // Clear keypad triggered
-    this.#currOperator = "";
-    this.#currNumber = 0;
-    const [...btns] = keypadAside.querySelectorAll(".item__btn");
+    this.currOperator = null;
+    this.currNumber = 0;
+    const btns = keypadAside.querySelectorAll(".item__btn");
     this._deactivateBtns(btns);
     this._updateDisplay("0");
   }
 
-  _handleAsideEvents(e) {
-    const clicked = e.target.closest(".item__btn");
+  _handleAsideEvents(e: Event) {
+    const clicked = (e.target as HTMLInputElement).closest(".item__btn");
     if (!clicked) return;
     // Operator keypad triggered
     if (!clicked.classList.contains("item__btn--equals"))
       return this._operatorTriggered.call(this, clicked);
     // Equals keypad triggered
-    if (!this.#currNumber) return;
+    if (!this.currNumber) return;
     const num1 = +labelDisplay.textContent;
-    const num2 = this.#currNumber;
-    const result = this._calcNumbers(this.#currOperator.value);
-    const [...btns] = keypadAside.querySelectorAll(".item__btn");
+    const num2 = this.currNumber;
+    const result = this._calcNumbers(this.currOperator.value as operator);
+    const btns = keypadAside.querySelectorAll(".item__btn");
     this._deactivateBtns(btns);
-    btnEquals.blur();
+    (btnEquals as HTMLElement).blur();
     btnEquals.classList.add("btn--active");
     this._updateDisplay(result);
     // Updating history tab
     const html = `
-      <p class="content__label label">${num2} ${
-      this.#currOperator.value
-    } ${num1} = ${result}
+      <p class="content__label label">${num2} ${this.currOperator.value} ${num1} = ${result}
       </p>
     `;
     // Inserting label onto history tab
     asideHistory.insertAdjacentHTML("beforeend", html);
   }
 
-  _handleKeyupPress(e) {
+  _handleKeyupPress(e: KeyboardEvent) {
     const key = e.key;
     if (!key) return;
     // Number keyboard input [0 - 9]
@@ -129,8 +127,6 @@ class App {
     switch (key) {
       case "Backspace":
         return this._updateDisplay(labelDisplay.textContent.slice(0, -1));
-      case "Enter":
-        return this._displayCalc();
       case "+":
         return this._operatorTriggered.call(this, btnPlus);
       case "-":
@@ -147,32 +143,32 @@ class App {
   /////////////////////////////////////
   //////////// Logic
 
-  _calcNumbers(op) {
+  _calcNumbers(operSign: operator) {
     const tempNum = +labelDisplay.textContent;
 
-    switch (op) {
+    switch (operSign) {
       case "+":
-        this.#currNumber = calculator.add(this.#currNumber, tempNum);
+        this.currNumber = calculator.add(this.currNumber, tempNum);
         break;
       case "-":
-        this.#currNumber = calculator.subtract(this.#currNumber, tempNum);
+        this.currNumber = calculator.subtract(this.currNumber, tempNum);
         break;
       case "*":
-        this.#currNumber = calculator.multiply(this.#currNumber, tempNum);
+        this.currNumber = calculator.multiply(this.currNumber, tempNum);
         break;
       case "/":
-        this.#currNumber = calculator.divide(this.#currNumber, tempNum);
+        this.currNumber = calculator.divide(this.currNumber, tempNum);
         break;
     }
 
-    return this.#currNumber;
+    return this.currNumber;
   }
 
-  _keypadTriggered(keypad) {
+  _keypadTriggered(keypad: HTMLInputElement) {
     if (btnEquals.classList.contains("btn--active")) return;
-    const [...btns] = keypadAside.querySelectorAll(".item__btn");
+    const btns = keypadAside.querySelectorAll(".item__btn");
     this._deactivateBtns(btns);
-    const keypadVal = keypad?.value ?? keypad;
+    const keypadVal = keypad?.value;
     const displayStr = labelDisplay.textContent;
 
     if (displayStr === "0") return this._updateDisplay(keypadVal);
@@ -180,20 +176,22 @@ class App {
     if (
       // Prevents > 1 decimal occurance in input string
       // and label exceeding > 13 characters
-      !(displayStr.includes(".") && keypadVal === ".") &&
+      !(displayStr.match(/\./g) && keypadVal === ".") &&
       displayStr.length <= 13
     )
       this._updateDisplay(displayStr.concat(keypadVal));
   }
 
-  _operatorTriggered(oper) {
-    this.#currOperator = oper;
+  _operatorTriggered(oper: HTMLInputElement) {
+    this.currOperator = oper;
 
-    if (this.#currNumber && !btnEquals.classList.contains("btn--active"))
-      this._calcNumbers(this.#currOperator.value);
-    else this.#currNumber = +labelDisplay.textContent;
+    if (this.currNumber && !btnEquals.classList.contains("btn--active")) {
+      this._calcNumbers(this.currOperator.value as operator);
+    } else {
+      this.currNumber = +labelDisplay.textContent;
+    }
 
-    const [...btns] = keypadAside.querySelectorAll(".item__btn");
+    const btns = keypadAside.querySelectorAll(".item__btn");
     this._deactivateBtns(btns);
     this._updateDisplay("0");
     oper.classList.add("btn--active");
